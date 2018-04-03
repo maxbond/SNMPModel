620ebc6f1736d3e6b6089dfe8e609bc26a57e01c
Maxim Bondarev <macintosh.way@gmail.com>
2018-04-03 09:31:01 +0500

    SNMPDevice class

--- src/SNMPModel.php
+++ src/SNMPModel.php
@@ -11,7 +11,7 @@ use Symfony\Component\Yaml\Yaml;
  * Class SNMPModel
  * Base SNMP model class.
  */
-class SNMPModel
+class SNMPModel extends SNMPDevice
 {
     const DEFAULT_RECEIVERS_CLASS_MAP = [
         'get' => ReceiveGet::class,
@@ -21,29 +21,6 @@ class SNMPModel
     const DEFAULT_MODIFIERS_CLASS_MAP = [
         'numeric' => ModifierNumeric::class,
     ];
-
-    const ONE_SECOND = 1000000;
-
-    /**
-     * SNMP version.
-     *
-     * @var int
-     */
-    protected $snmpVersion = \SNMP::VERSION_2c;
-
-    /**
-     * Connection timeout.
-     *
-     * @var int
-     */
-    protected $snmpTimeout = self::ONE_SECOND;
-
-    /**
-     * SNMP community.
-     *
-     * @var string
-     */
-    protected $snmpCommunity;
 
     /**
      * Model scheme.
@@ -72,32 +49,6 @@ class SNMPModel
      * @var array
      */
     protected $modifiersClasses;
-
-    /**
-     * Enable quick print ?
-     *
-     * @var bool
-     */
-    protected $quickPrint = true;
-
-    /**
-     * Enable enum print ?
-     *
-     * @var bool
-     */
-    protected $enumPrint = true;
-
-    /**
-     * SNMP decice IP address.
-     *
-     * @var string
-     */
-    protected $ip;
-
-    /**
-     * @var \SNMP
-     */
-    protected $snmpSession;
 
     /**
      * @var \Maxbond\SNMPModel\ReceiversClassMap
@@ -168,49 +119,6 @@ class SNMPModel
     public function setModel(array $model): void
     {
         $this->model = $model;
-    }
-
-    /**
-     * Initialize SNMP session.
-     *
-     * @throws \Maxbond\SNMPModel\SNMPModelException
-     */
-    public function initSNMPSession(): void
-    {
-        try {
-            $this->snmpSession = new \SNMP($this->snmpVersion, $this->ip, $this->snmpCommunity, $this->snmpTimeout);
-            $this->snmpSession->exceptions_enabled = true;
-            $this->snmpSession->oid_output_format = SNMP_OID_OUTPUT_NUMERIC;
-
-            if (true === $this->quickPrint) {
-                $this->snmpSession->quick_print = true;
-            }
-            if (true === $this->enumPrint) {
-                $this->snmpSession->enum_print = true;
-            }
-        } catch (\Exception $e) {
-            throw new SNMPModelException($e->getMessage());
-        }
-    }
-
-    /**
-     * Get SNMP session
-     *
-     * @return null|\SNMP
-     */
-    public function getSNMPSession(): ?\SNMP
-    {
-        return $this->snmpSession;
-    }
-
-    /**
-     * Set SNMP session
-     *
-     * @param \SNMP $snmpSession
-     */
-    public function setSNMPSession(\SNMP $snmpSession): void
-    {
-        $this->snmpSession = $snmpSession;
     }
 
     /**
